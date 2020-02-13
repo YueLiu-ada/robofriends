db75d9857bcf5deb1fb427a38169308f11c7d600
YueLiu-ada <59384345+YueLiu-ada@users.noreply.github.com>
2020-02-13 17:57:27 -0500

    final

---
+++ build/precache-manifest.c56f2388541068a88ee35c1f2480a781.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ca41af07a63ee1de361c54047c3e9c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c8890da5dfac99701af1",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "516a555b8bea3b9a85c3",
+    "url": "/robofriends/static/css/main.ad9827da.chunk.css"
+  },
+  {
+    "revision": "c8890da5dfac99701af1",
+    "url": "/robofriends/static/js/2.a98415d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.a98415d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "516a555b8bea3b9a85c3",
+    "url": "/robofriends/static/js/main.36a4b8f0.chunk.js"
+  },
+  {
+    "revision": "e6e9e20fe23598f77326",
+    "url": "/robofriends/static/js/runtime-main.d4e5f5ba.js"
+  },
+  {
+    "revision": "2e5ec5b3e538df515284bcc246b0629d",
+    "url": "/robofriends/static/media/Strawberry Shortcake Supplement Logo.2e5ec5b3.woff"
+  }
+]);
